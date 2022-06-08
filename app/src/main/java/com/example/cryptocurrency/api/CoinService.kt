9e2c4ace01ef920c2b54347e02bff81d83c1bc30
Mohammad Shadab <sadabkhan90063@gmail.com>
2022-06-08 07:56:00 +0530

    two new classes added CoinService object class and CoinApiInterface

--- app/src/main/java/com/example/cryptocurrency/api/CoinService.kt
+++ app/src/main/java/com/example/cryptocurrency/api/CoinService.kt
@@ -3,12 +3,13 @@ package com.example.cryptocurrency.api
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object ApiUtilities {
-    fun getInstance() : Retrofit {
-        return Retrofit.Builder()
+object CoinService {
+    val apiInstance : CoinApiInterface
+    init {
+        val retrofit = Retrofit.Builder()
             .baseUrl("https://api.coinmarketcap.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+        apiInstance = retrofit.create(CoinApiInterface::class.java)
     }
-
 }
--- app/src/main/java/com/example/cryptocurrency/fragment/TopLossGainFragment.kt
+++ app/src/main/java/com/example/cryptocurrency/fragment/TopLossGainFragment.kt
@@ -1,6 +1,7 @@
 package com.example.cryptocurrency.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,11 +13,16 @@ import com.example.cryptocurrency.R
 import com.example.cryptocurrency.adapter.MarketRecyclerViewAdapter
 import com.example.cryptocurrency.api.ApiInterface
 import com.example.cryptocurrency.api.ApiUtilities
+import com.example.cryptocurrency.api.CoinService
 import com.example.cryptocurrency.databinding.FragmentTopLossGainBinding
 import com.example.cryptocurrency.model.CryptoCurrency
+import com.example.cryptocurrency.model.MarketModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.create
 import java.util.*
 import kotlin.collections.ArrayList
@@ -34,7 +40,7 @@ class TopLossGainFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_top_loss_gain, container, false)
         mAdapter = MarketRecyclerViewAdapter("Home")
         binding.topGainLoseRecyclerView.adapter = mAdapter
-        getMarketData()
+        getMarketCoinDataWithSingleton()
 
         return binding.root
     }
@@ -76,6 +82,53 @@ class TopLossGainFragment : Fragment() {
 
         }
     }
+
+    private fun getMarketCoinDataWithSingleton(){
+
+            val coins = CoinService.apiInstance.getMarketCoinData()
+
+
+                coins.enqueue(object : Callback<MarketModel>{
+                    override fun onResponse(call: Call<MarketModel>, response: Response<MarketModel>) {
+                        Log.d("TopLossGainFragment", "Success")
+                        val res = response
+                        if (res.body() != null){
+                            val position = requireArguments().getInt("position")
+                            val dataItem = res.body()!!.data.cryptoCurrencyList
+                            Collections.sort(dataItem){
+                                    item1 , item2 -> (item2.quotes[0].percentChange24h.toInt())
+                                .compareTo(item1.quotes[0].percentChange24h.toInt())
+                            }
+
+                            val list = ArrayList<CryptoCurrency>()
+
+
+                            if (position == 0){
+                                list.clear()
+                                for ( i in 0..19){
+                                    list.add(dataItem[i])
+                                }
+                                mAdapter.submitList(list)
+                                binding.spinKitView.visibility = GONE
+                            }else{
+                                list.clear()
+                                for ( i in 0..19){
+                                    list.add(dataItem[dataItem.size - 1 - i])
+                                }
+                                mAdapter.submitList(list)
+                                binding.spinKitView.visibility = GONE
+                            }
+                        }
+                    }
+
+                    override fun onFailure(call: Call<MarketModel>, t: Throwable) {
+                        Log.d("TopLossGainFragment", t.toString())
+                    }
+                })
+
+
+    }
+
 
     override fun onResume() {
         super.onResume()
